0af0a3e1d58490fb0684da0e50da50300aa1cdf4
Alan Greene x00124937 <x00124937@mytudublin.ie>
2019-09-24 11:29:51 +0100

    added in test class and functions

--- pages_project/pages/tests.py
+++ pages_project/pages/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
+
+#TestCase is used for testing when databases are involved
+#SimpleTestCase can be used when there are no databases involved
 
 # Create your tests here.
+#These tests simply test whether or not the webpages exist.
+
+class SimpleTests(SimpleTestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_about_page_status_code(self):
+        response = self.client.get('/about/')
+        self.assertEqual(response.status_code, 200)
